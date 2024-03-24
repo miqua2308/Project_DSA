328b5d1515df08c779a88bdf9b94c1c6efa831b0
Quân Red <miqua2004@gmail.com>
2024-03-24 15:23:13 +0700

    adding motion

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -68,21 +68,57 @@ public class Player extends Entity{
             direction = "left";
             x -= speed;
         }
+
+        spriteCounter++;
+        if (spriteCounter>10){
+            if(spriteNum ==  1){
+                spriteNum = 2;
+            } else if (spriteNum == 2) {
+                spriteNum = 3;
+            }
+            else if (spriteNum == 3 ){
+                spriteNum = 1;
+            }
+        }
     }
     public void draw(Graphics2D g2){
         BufferedImage image = null;
         switch (direction){
             case "up":
-                image = up1;
+                if (spriteNum == 1){
+                    image = up1;}
+                if (spriteNum == 2){
+                    image = up2;}
+                if (spriteNum == 3){
+                    image = up3;
+                }
                 break;
             case "down":
-                image = down1;
+                if (spriteNum == 1){
+                    image = down1;}
+                if (spriteNum == 2){
+                    image = down2;}
+                if (spriteNum == 3){
+                    image = down3;
+                }
                 break;
             case "right":
-                image = right1;
+                if (spriteNum == 1){
+                    image = right1;}
+                if (spriteNum == 2){
+                    image = right2;}
+                if (spriteNum == 3){
+                    image = right3;
+                }
                 break;
             case "left":
-                image = left1;
+                if (spriteNum == 1){
+                    image = left1;}
+                if (spriteNum == 2){
+                    image = left2;}
+                if (spriteNum == 3){
+                    image = left3;
+                }
                 break;
         }
         g2.drawImage(image ,x,y,gp.tileSize,gp.tileSize,null);
